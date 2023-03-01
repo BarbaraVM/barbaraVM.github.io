9142296e44893a6b918912d100e68f7ddeb2eacf
angelvoisard <angelvoisard@gmail.com>
2023-03-01 00:12:48 -0300

    primer commit

---
+++ proyectocv.js
@@ -0,0 +1,26 @@
+function mouseEnter() {
+    document.getElementById("demo").style.color = "red";
+  }
+  
+  function mouseLeave() {
+    document.getElementById("demo").style.color = "black";
+  }
+
+
+function ingresa() {
+  document.getElementById("demo1").style.color = "yellow";
+  }
+  
+  function sale() {
+    document.getElementById("demo1").style.color = "black";
+  }
+
+
+function mouseSobre() {
+    document.getElementById("demo2").style.color = "blue";
+    }
+    
+    
+    function mouseFuera() {
+      document.getElementById("demo2").style.color = "black";
+    }
